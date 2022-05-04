f16dab8991a796d60e9977a1c487fc592756e877
George Leslie-Waksman <waksman@gmail.com>
2022-05-04 00:13:13 -0700

    GitHub pages based deploy

--- conf.py
+++ conf.py
@@ -675,18 +675,22 @@ REDIRECTIONS = []
 # You will need to configure the deployment branch on GitHub.
 # GITHUB_SOURCE_BRANCH = 'src'
 # GITHUB_DEPLOY_BRANCH = 'master'
+GITHUB_SOURCE_BRANCH = 'trunk'
+GITHUB_DEPLOY_BRANCH = 'deploy'
 
 # The name of the remote where you wish to push to, using github_deploy.
 # GITHUB_REMOTE_NAME = 'origin'
+GITHUB_REMOTE_NAME = 'ghpages'
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
 # GITHUB_COMMIT_SOURCE = True
+GITHUB_COMMIT_SOURCE = False
 
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
 # to the location of conf.py
-OUTPUT_FOLDER = 'static'
+OUTPUT_FOLDER = 'output'
 
 # where the "cache" of partial generated content should be located
 # default: 'cache'
